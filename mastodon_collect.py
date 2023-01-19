1678cb2677dfac31d88c2a397141abb28fe42ed7
Yunzhi Gao <gaoyunzhi@gmail.com>
2023-01-19 11:05:56 -0800

    commit

--- mastodon_collect.py
+++ mastodon_collect.py
@@ -5,6 +5,8 @@ import plain_db
 import yaml
 from mastodon import Mastodon
 from telegram.ext import Updater
+from bs4 import BeautifulSoup
+import time
 
 existing = plain_db.load('existing')
 with open('credential') as f:
@@ -12,6 +14,26 @@ with open('credential') as f:
 tele = Updater(credential['bot_token'], use_context=True)
 debug_group = tele.bot.get_chat(credential['debug_group'])
 tele_channel = tele.bot.get_chat(credential['tele_channel'])
+timer = {}
+def wait(key, sec):
+    time.sleep(max(0, timer.get(key, 0) - time.time()))
+    timer[key] = time.time() + sec
+
+def getMarkdownMessage(reply_url, parent_url, content):
+    return '%s [origin](%s) [source](%s)' % (content, parent_url, reply_url)
+
+def getHtmlMessage(reply_url, parent_url, content):
+    return '%s <a href=%s>origin</a> <a href=%s>source</a>' % (content, parent_url, reply_url)
+
+def getPlainMessage(reply_url, parent_url, content):
+    return '%s origin: %s source: %s' % (content, parent_url, reply_url)
+
+
+def getContentText(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    for item in soup.find_all('span', class_='h-card'):
+        item.decompose()
+    return soup.text
 
 def run():
     for mastodon_name in credential['mastodon_users']:
@@ -21,8 +43,26 @@ def run():
         )
         statuses = mastodon.account_statuses(mastodon.me().id)
         for status in statuses:
-            print(mastodon.status_context(status.id).descendants)
-        return # testing
+            parent_url = status.url
+            for reply_status in mastodon.status_context(status.id).descendants:
+                reply_url = reply_status.url
+                if existing.get(reply_url):
+                    continue
+                content = getContentText(reply_status.content)
+                message_markdown = getMarkdownMessage(reply_url, parent_url, content)
+                message_html = getHtmlMessage(reply_url, parent_url, content)
+                message_plain = getPlainMessage(reply_url, parent_url, content)
+                wait('tele_channel', 5)
+                try:
+                    tele_channel.send_message(message_markdown, parse_mode='Markdown')
+                except Exception as e:
+                    print('mastodon_collect markdown send fail', e, reply_url)
+                    try:
+                        tele_channel.send_message(message_html, parse_mode='HTML')
+                    except Exception as e:
+                        print('mastodon_collect html send fail', e, reply_url)
+                        tele_channel.send_message(message_plain)
+                existing.update(reply_url, 1)
         
 if __name__ == '__main__':
     run()
